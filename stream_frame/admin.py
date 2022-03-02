7bb6dc44bc05fb4ec5138d702cf90af95d6c98d6
Paul Bailey <paul@neutron.studio>
2022-03-01 21:55:52 -0600

    auth flow

--- stream_frame/admin.py
+++ stream_frame/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from stream_frame.models import OAuthCredentials
 
 
 @admin.register(OAuthCredentials)
 class OAuthAdmin(admin.ModelAdmin):
-  list_display = ('name', 'modified')
+  list_display = ('name', 'modified', 'authorize')
   list_filter = ('modified',)
+
+  def authorize(self, obj):
+    if obj and not obj.token:
+      return mark_safe(f'<a href="/stream-frame/start-auth/{obj.id}/">Authorize</a>')
